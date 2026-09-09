31ce2ae7f36660df34415dc1b09573562eb99b6f
NICOLAS CLAUDINO DE AGUIAR <25162441@edu119.local>
2026-09-09 16:42:39 -0300

    Arrumando a tela inicial

--- src/componentes/CardInicial/index.tsx
+++ src/componentes/CardInicial/index.tsx
@@ -1,64 +1,42 @@
 import styles from './styles.module.css';
 import type { ComponentProps, ReactNode } from 'react';
 
-type CardDispositivoProps = {
+type CardInicialProps = {
   icone: ReactNode;
   texto: string;
   informacao?: string;
 } & ComponentProps<"div">;
 
-export function CardDispositivo({
+export function CardInicial({
   icone,
   texto,
   informacao = 'i',
   ...props
-}: CardDispositivoProps) {
+}: CardInicialProps) {
   return (
-    <div
-      className={`${styles.dispositivo}`}
-      {...props}
-    >
-      <div className={`${styles.dispositivoIcone}`}>
-        {icone}
+    <div className={`${styles.dispositivo}`} {...props}>
+      <div className={`${styles.IconeCompleto}`}>
+        <div className={`${styles.Icone}`}>
+          {icone}
+        </div>
 
-        <div className={`${styles.dispositivoSinal}`}>
-          <svg
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M2 8.5C7.5 3.5 16.5 3.5 22 8.5"
-              fill="none"
-              stroke="#a85b16"
-              strokeWidth="2"
-              strokeLinecap="round"
-            />
-
-            <path
-              d="M5.5 12C9 8.8 15 8.8 18.5 12"
-              fill="none"
-              stroke="#a85b16"
-              strokeWidth="2"
-              strokeLinecap="round"
-            />
-
-            <path
-              d="M9 15.5C10.8 13.8 13.2 13.8 15 15.5"
-              fill="none"
-              stroke="#a85b16"
-              strokeWidth="2"
-              strokeLinecap="round"
-            />
-
-            <circle
-              cx="12"
-              cy="19"
-              r="1.5"
-              fill="#a85b16"
-            />
+        <div className={`${styles.IconeWifi}`}>
+          <svg xmlns="http://www.w3.org/2000/svg" 
+            width="24" height="24" viewBox="0 0 24 24" 
+            fill="none" stroke="currentColor" 
+            stroke-width="2" 
+            stroke-linecap="round" 
+            stroke-linejoin="round"
+            color="#ffffff" 
+            className="lucide lucide-wifi">
+            <path d="M12 20h.01" />
+            <path d="M2 8.82a15 15 0 0 1 20 0" />
+            <path d="M5 12.859a10 10 0 0 1 14 0" /><path d="M8.5 16.429a5 5 0 0 1 7 0" />
           </svg>
         </div>
+
       </div>
+
 
       <span className={`${styles.dispositivoTexto}`}>
         {texto}
