8f194cd7cfa8f12fa885cb5c56314a3c7e0c21db
nicolascda <125214956+nicolascda@users.noreply.github.com>
2026-09-16 14:57:44 -0300

    Fetch em /usuarios

--- src/paginas/registrar/index.tsx
+++ src/paginas/registrar/index.tsx
@@ -33,7 +33,7 @@ export function TelaDeRegistrar() {
       setCarregando(true);
 
       const resposta = await fetch(
-        "/api/usuarios",
+        "/usuarios",
         {
           method: "POST",
           headers: {
