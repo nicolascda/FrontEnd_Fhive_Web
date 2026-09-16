fd36b45ae88aff49fcb8bae7d84c69c05913010c
nicolascda <125214956+nicolascda@users.noreply.github.com>
2026-09-16 11:13:21 -0300

    Link correto do ip agora da Aws

--- src/paginas/registrar/index.tsx
+++ src/paginas/registrar/index.tsx
@@ -33,7 +33,7 @@ export function TelaDeRegistrar() {
       setCarregando(true);
 
       const resposta = await fetch(
-        "http://10.0.1.10:3000/usuarios",
+        "http://10.0.1.20:3000/usuarios",
         {
           method: "POST",
           headers: {
